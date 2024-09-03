2439d2d11d18f03cef734fd20493b58a9c7b9415
Zoraiz <44453814+sensehat@users.noreply.github.com>
2024-09-03 00:46:53 -0400

    Update ftpbrute.py

--- Project/ftpbrute.py
+++ Project/ftpbrute.py
@@ -1,57 +1,7 @@
 #!/usr/bin/python3
 from ftplib import FTP
-import socket, argparse
+import socket
 
-def main():
-    print("""___________                         _____                    
-\_   _____/__________   ____  _____/ ____\_ __  ______ ____  
- |    __)/  _ \_  __ \_/ ___\/ __ \   __\  |  \/  ___// __ \ 
- |     \(  <_> )  | \/\  \__\  ___/|  | |  |  /\___ \\  ___/ 
- \___  / \____/|__|    \___  >___  >__| |____//____  >\___  >
-     \/                    \/    \/                \/     \/
-
----------------------------------------------------------
-|      A Python SSH and FTP brute force tool by         |
----------------------------------------------------------
-
-Choose your target:
-
-[1] Crack SSH
-      - Bruteforce SSH logins using a customizable wordlist.
-      - Supports multiple threads for faster cracking.
-      
-[2] Crack FTP
-      - Bruteforce FTP logins with your chosen dictionary.
-      - Includes anonymous login bypass checks.
-
----------------------------------------------------------
-Select an option by entering the corresponding number:
-""")
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", type=str, dest="target", help="--target ip", default=None)
-    parser.add_argument("-w", type=str, dest="wordlist", help="--wordlist", default=None)
-    parser.add_argument("-u", type=str, dest="username", help='--username', default=None)
-    parser.add_argument("-c", type=str, dest="check", help="--check", default=None)
-    parser.add_argument("-a", type=str, dest="anon", help="--anonymous", default=None)
-    
-    args = parser.parse_args()
-
-    target = args.target
-    username = args.username
-    wordlist = args.wordlist
-    check = args.check
-    anon = args.anon
-
-    if anon:
-        Anon_login(anon)
-    elif target and username and wordlist:
-        Brute_force(target, username, wordlist)
-    elif check:
-        Port_21(check)
-    else:
-        print("[-] Not a valid option, please refer to -h for help")
-  
 def Anon_login(target):
     print("[*] Trying anonymous login...")
     try:
@@ -91,6 +41,3 @@ def Login_ftp(target, username, word):
     except:
         ftp_session.quit()
         print("[-] Password failed")
-
-main()
-print("[*] The FTP brute tool has finished!")
